e712c068554527f418784fd2963b86d9184be03a
nowoterra-oss <nowoterra@gmail.com>
2025-11-20 22:18:00 +0300

    code

---
+++ src/EduPortal.Infrastructure/ApplicationDbContextFactory.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace EduPortal.Infrastructure.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // API projesinin klasör yoluna git
+            var basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\EduPortal.API"));
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
